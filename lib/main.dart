353926d350119fb8160dc78596d1d7d731bd2d7e
gitpher <jun_chois@naver.com>
2022-11-12 11:12:25 +0900

    showing quiz even when New_Quiz_button isn't pressed yet

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String quiz = "";
 
+  @override
+  void initState() {
+    super.initState();
+    getQuiz();
+  }
+
+  /// 퀴즈 가져오기
+  void getQuiz() async {
+    String trivia = await getNumberTrivia();
+    setState(() {
+      quiz = trivia;
+    });
+  }
+
   /// Numbers API 호출하기
   Future<String> getNumberTrivia() async {
     // get 메소드로 URL 호출
@@ -43,49 +57,49 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        color: Colors.blueAccent,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // quiz
-            Expanded(
-              child: Center(
-                child: Text(
-                  quiz,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 28,
-                    color: Colors.white,
+      backgroundColor: Colors.blueAccent,
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          color: Colors.blueAccent,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // quiz
+              Expanded(
+                child: Center(
+                  child: Text(
+                    quiz,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 28,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-            // New Quiz 버튼
-            SizedBox(
-              height: 42,
-              child: ElevatedButton(
-                child: Text(
-                  "New Quiz",
-                  style: TextStyle(
-                    color: Colors.blueAccent,
-                    fontSize: 24,
+              // New Quiz 버튼
+              SizedBox(
+                height: 42,
+                child: ElevatedButton(
+                  child: Text(
+                    "New Quiz",
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 24,
+                    ),
                   ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                  ),
+                  onPressed: () {
+                    // New Quiz 클릭시 퀴즈 가져오기
+                    getQuiz();
+                  },
                 ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.white),
-                ),
-                onPressed: () async {
-                  // New Quiz 클릭시 퀴즈 가져오기
-                  String trivia = await getNumberTrivia();
-                  setState(() {
-                    quiz = trivia;
-                  });
-                },
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
